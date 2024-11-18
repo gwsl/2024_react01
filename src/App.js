8d61e5aeabcecf042a6617b9211c55a5cf67c978
davidgw <coreax7@gmail.com>
2024-11-18 16:21:56 +0900

    fix error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CourseListCard from './components/course/CourseListCard';
 
 
 // 파스칼 케이스 사용해야 된다. (첫글자 대문저 단어 첫글자 대문자)
@@ -10,10 +11,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
+    <main>
+      
         <CourseListCard />
 
-    </div>
+    </main>
+    
   );
 }
 
